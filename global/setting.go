51ebe528beffb4939c02d33f869d76e6d0623796
Jenkin Lee <zhongjianlee@outlook.com>
2021-01-31 16:45:23 +0800

    feat: 配置管理

---
+++ global/setting.go
@@ -0,0 +1,9 @@
+package global
+
+import "blog_server/pkg/setting"
+
+var (
+	ServerSetting   *setting.ServerSettingS
+	AppSetting      *setting.AppSettingS
+	DatabaseSetting *setting.DatabaseSettingS
+)
--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"blog_server/global"
+	"blog_server/pkg/setting"
 	"blog_server/routers"
+	"log"
 	"net/http"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init setupSetting err: %v", err)
+	}
+}
+
 func main() {
+	gin.SetMode(global.ServerSetting.RunMode)
 	router := routers.NewRouter()
 
 	s := &http.Server{
@@ -17,4 +30,26 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 	s.ListenAndServe()
+}
+
+func setupSetting() error {
+	setting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
 }
